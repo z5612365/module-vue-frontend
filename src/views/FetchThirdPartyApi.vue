c19c7c829cf8024bde702e31721025d43b656ba1
matt <contact@mattc.art>
2021-07-27 08:27:56 +0800

    init

---
+++ src/views/FetchThirdPartyApi.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="fetchThirdPartyApi">
+    <button v-on:click="fetchThirdPartyApi">fetchThirdPartyApi</button>
+  <div>{{text}}</div>
+
+  </div>
+</template>
+
+<script>
+const axios = require('axios');
+export default {
+  data: function(){
+    return {
+      text: "content",
+    };
+  },
+  methods:{
+        fetchThirdPartyApi: function () { 
+          this.text = axios.get('https://quoters.apps.pcfone.io/api/random')
+            .then(function (response) {
+              // handle success
+              console.log(response);
+            })
+            .catch(function (error) {
+              // handle error
+              console.log(error);
+            })
+            .then(function () {
+              // always executed
+            });
+        }
+  },
+  mounted:function(){},
+}
+</script>
+
+
